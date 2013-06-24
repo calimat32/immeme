436872395991a067d9902ceb65261bc941442513
Juan Pablo Santos <santos.juanpablo@gmail.com>
2013-06-24 00:02:19 -0500

    Working shadows

--- caption.py
+++ caption.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
 
 def write_on_image(filename, top, bottom):
     background = Image.open("resources/" + filename)
@@ -10,19 +10,33 @@ def write_on_image(filename, top, bottom):
 
     font = ImageFont.truetype("resources/impact.ttf", 36)
 
-    top_location = (get_center_position(top, background) , 5)
+    top_location = (get_center_position(top, background), 5)
     bottom_location = (get_center_position(bottom, background), height-70)
 
+    background.paste(draw_shadow(top), (top_location[0]-5, top_location[1]-5), mask=draw_shadow(top))
+    background.paste(draw_shadow(bottom), (bottom_location[0]-5, bottom_location[1]-5), mask=draw_shadow(bottom))
 
     drawer.text(top_location, top, "white", font=font)
     drawer.text(bottom_location, bottom, "white", font=font)
 
     return background
 
-def get_center_position(text,image):
+def get_center_position(text, image):
     drawer = ImageDraw.Draw(image)
     font = ImageFont.truetype("resources/impact.ttf", 36)
     from_left = int((image.size[0]/2) - (drawer.textsize(text, font)[0]/2))
 
     return from_left
+
+def draw_shadow(text):
+    dummy = Image.new("RGBA", (100,100), (0,0,0,0))
+    dummy_draw = ImageDraw.Draw(dummy)
+    font = ImageFont.truetype("resources/impact.ttf", 36)
+    width, height = dummy_draw.textsize(text,font)
+
+    canvas = Image.new("RGBA", (width+10, height+10))
+    draw = ImageDraw.Draw(canvas)
+    draw.text((5,5), text, "black", font=font)
+    blured = canvas.filter(ImageFilter.GaussianBlur(5))
+    return blured
 
--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
 import caption, sys
+from PIL import ImageFilter
 
 caption.write_on_image("wonka.jpg", "Writing a program to make memes?", "I bet nobody has done that before").show()
+
+#caption.draw_shadow("Longer text").save("test.png", "PNG")
