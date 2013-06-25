41ee12eaaf5603d23472ca4f251c9b177ec21172
Juan Pablo Santos <santos.juanpablo@gmail.com>
2013-06-25 17:14:22 -0500

    Organize source folder

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-import caption, sys
+import imgbackend.caption as caption
+import sys
 from PIL import ImageFilter
 
 caption.write_on_image("wonka.jpg", "Writing a program to make memes?", "I bet nobody has attempted such a massive feat before").show()
